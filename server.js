7d1b2b56554fbf39503d64937361da3057db321d
Moiz Ansari <moizansari.india@gmail.com>
2025-04-12 21:50:04 +0530

    creating user Route

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import express from 'express'
 const app = express();
 const PORT = 5000;
 
-app.get('/api/users', (req, res) => {
+app.get('/hitesh', (req, res) => {
     res.send('Hello Coming from Users')
+});
+
+app.get("/moiz", (req, res) => {
+
 })
 
 app.listen(PORT, () => {
